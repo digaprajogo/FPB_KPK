a3f96720c6d5e67180567b551ed0ddf6dddf8679
digaprajogo <prajogodiga@gmail.com>
2018-11-06 09:42:56 +0700

    Ujian Fundamental Soal1 FPB & KPK

---
+++ Soal1.js
@@ -0,0 +1,18 @@
+// FPB & KPK
+
+function fpb_kpk(x,y){
+    var FPB=0;
+    
+    for(var c=1; c<=x; c++){
+        var d = x%c
+        var e = y%c
+        if(d==e && e==0){
+            var FPB = c
+        }
+    }
+    var KPK = (x*y)/FPB
+    
+    console.log('FPB ' + x + ' & ' + y + ' = ' + FPB)
+    console.log('KPK ' + x + ' & ' + y + ' = ' + KPK)
+}
+fpb_kpk(2,3)
